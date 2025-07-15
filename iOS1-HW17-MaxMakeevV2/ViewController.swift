9f138e8db826823773b0a17b092e881305f23c5d
DwightShrut <skatemax@mail.ru>
2025-07-15 19:45:24 +0300

    Add background on ImageView
    Add background on imageView and add constraints

--- iOS1-HW17-MaxMakeevV2/ViewController.swift
+++ iOS1-HW17-MaxMakeevV2/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // Mark: - ImageView
+    private lazy var imageView: UIImageView = {
+        let image = UIImage(named:"background")
+        let imageView = UIImageView(image:image)
+        imageView.contentMode = .scaleToFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupHierarchy()
+        setupApp()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func setupHierarchy() {
+        view.addSubview(imageView)
+    }
+    
+    private func setupApp() {
+        imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
+        imageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        imageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        
+    }
+    
+    @objc private func buttonPressed() {
+        
+    }
 }
 
