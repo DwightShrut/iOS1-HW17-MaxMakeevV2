ca26396e0e5d314b81f0edc5c312a8ec067e8b2c
DwightShrut <skatemax@mail.ru>
2025-07-16 16:19:10 +0300

    change View background color

--- iOS1-HW17-MaxMakeevV2/ViewController.swift
+++ iOS1-HW17-MaxMakeevV2/ViewController.swift
@@ -209,6 +209,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .white
         setupHierarchy()
         setupApp()
         // Do any additional setup after loading the view.
