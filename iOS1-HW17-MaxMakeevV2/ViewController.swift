9d5195b8cecb7f19e7ed745e79c7f2a019ef8f5e
DwightShrut <skatemax@mail.ru>
2025-07-23 00:49:31 +0300

    Recreate view elements with methods

--- iOS1-HW17-MaxMakeevV2/ViewController.swift
+++ iOS1-HW17-MaxMakeevV2/ViewController.swift
@@ -91,193 +91,146 @@ class ViewController: UIViewController {
     }()
     
     //MARK: - Login Label
-    
-    private lazy var loginLabel: UILabel = {
-        let loginLabel = UILabel()
-        loginLabel.text = "Login"
-        loginLabel.textColor = .white
-        loginLabel.font = .systemFont(ofSize: 35, weight: .regular)
-        loginLabel.textAlignment = .center
-        loginLabel.translatesAutoresizingMaskIntoConstraints = false
-        return loginLabel
-    }()
+    private lazy var loginLabel = createLabel(
+        text: "Login",
+        fontSize: 35,
+        color: .white,
+        alignment: .center
+    )
     
     //MARK: - Login Stack
     
-    private lazy var loginStackView: UIStackView = {
-        let loginStackView = UIStackView(arrangedSubviews: [loginTextField, passwordTextField])
-        loginStackView.axis = .vertical
-        loginStackView.distribution = .fillEqually
-        loginStackView.spacing = 11
-        loginStackView.contentMode = .scaleToFill
-        loginStackView.translatesAutoresizingMaskIntoConstraints = false
-        return loginStackView
-    }()
+    private lazy var loginStackView = createStackView(
+        axis: .vertical,
+        distribution: .fillEqually,
+        spacing: 11,
+        alignment: nil,
+        contentMode: .scaleToFill
+    )
     
-    private lazy var loginTextField: UITextField = {
-        let loginTextField = UITextField()
-        loginTextField.placeholder = "max@icloud.com"
-        loginTextField.textColor = .black
-        loginTextField.font = .systemFont(ofSize: 14)
-        loginTextField.layer.cornerRadius = 20
-        loginTextField.backgroundColor = .white
-        loginTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        loginTextField.setLeftIcon(UIImage(systemName: "person.circle")!)
-        let rightIcon = UIImage(named: "right")!
-        loginTextField.setRightIcon(rightIcon)
-        return loginTextField
-    }()
     
-    private lazy var passwordTextField: UITextField = {
-        let passwordTextField = UITextField()
-        passwordTextField.placeholder = "Password"
-        passwordTextField.textColor = .black
-        passwordTextField.font = .systemFont(ofSize: 14)
-        passwordTextField.layer.cornerRadius = 20
-        passwordTextField.backgroundColor = .white
-        passwordTextField.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        passwordTextField.setLeftIcon(UIImage(systemName: "lock")!)
-        return passwordTextField
-    }()
+    private lazy var loginTextField = createTextField(
+        placeholder: "max@icloud.com",
+        textColor: .black,
+        backgroundColor: .white,
+        fontSize: 14,
+        cornerRadius: 20,
+        leftIcon: .init(systemName: "person.circle"),
+        rightIcon: .right
+    )
     
-    //MARK: LoginButton Stack
+    private lazy var passwordTextField = createTextField(
+        placeholder: "Password",
+        textColor: .black,
+        backgroundColor: .white,
+        fontSize: 14,
+        cornerRadius: 20,
+        leftIcon: .init(systemName: "lock"),
+        rightIcon: nil
+    )
     
-    private lazy var loginButtonStackView: UIStackView = {
-        let loginButtonStackView = UIStackView(arrangedSubviews: [loginButton, forgotPasswordButton])
-        loginButtonStackView.axis = .vertical
-        loginButtonStackView.distribution = .fillProportionally
-        loginButtonStackView.spacing = 10
-        loginButtonStackView.contentMode = .scaleToFill
-        loginButtonStackView.translatesAutoresizingMaskIntoConstraints = false
-        return loginButtonStackView
-    }()
+    //MARK: - LoginButton Stack
     
-    private lazy var loginButton: UIButton = {
-        let loginButton = UIButton(type: .system)
-        loginButton.layer.cornerRadius = 20
-        loginButton.setTitle("Login", for: .normal)
-        loginButton.setTitleColor(.white, for: .normal)
-        loginButton.backgroundColor = #colorLiteral(red: 0.4199445248, green: 0.3708131313, blue: 1, alpha: 1)
-        loginButton.configuration?.cornerStyle = .capsule
-        loginButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        return loginButton
-    }()
+    private lazy var loginButtonStackView = createStackView(
+        axis: .vertical,
+        distribution: .fillProportionally,
+        spacing: 10,
+        alignment: nil,
+        contentMode: .scaleToFill
+    )
     
-    private lazy var forgotPasswordButton: UIButton = {
-        let forgotPasswordButton = UIButton(type: .system)
-        forgotPasswordButton.setTitle("Forgot your password?", for: .normal)
-        forgotPasswordButton.setTitleColor(.white, for: .normal)
-        return forgotPasswordButton
-    }()
+    private lazy var loginButton = createButton(
+        title: "Login",
+        textColor: .white,
+        backgroundColor: #colorLiteral(red: 0.4199445248, green: 0.3708131313, blue: 1, alpha: 1),
+        icon: nil
+    )
+    
+    private lazy var forgotPasswordButton = createButton(
+        title: "Forgot your password?",
+        textColor: .white,
+        backgroundColor: nil,
+        icon: nil
+    )
     
     //MARK: - "Connect with" Stack
     
-    private lazy var connectWithStack: UIStackView = {
-        let connectWithStack = UIStackView(arrangedSubviews: [line1, connectWithnLabel, line2])
-        connectWithStack.axis = .horizontal
-        connectWithStack.alignment = .center
-        connectWithStack.distribution = .fillEqually
-        connectWithStack.spacing = 8
-        connectWithStack.contentMode = .scaleAspectFit
-        connectWithStack.translatesAutoresizingMaskIntoConstraints = false
-        return connectWithStack
-    }()
+    private lazy var connectWithStack = createStackView(
+        axis: .horizontal,
+        distribution: .fillEqually,
+        spacing: 8,
+        alignment: .center,
+        contentMode: .scaleAspectFit
+    )
     
-    private lazy var line1: UIButton = {
-        let line1 = UIButton(type: .system)
-        line1.layer.cornerRadius = 20
-        line1.backgroundColor = .lightGray
-        line1.configuration?.cornerStyle = .capsule
-        line1.heightAnchor.constraint(equalToConstant: 2).isActive = true
-        return line1
-    }()
+    private lazy var line1 = createButton(
+        title: nil,
+        textColor: nil,
+        backgroundColor: .lightGray,
+        icon: nil
+    )
     
-    private lazy var line2: UIButton = {
-        let line2 = UIButton(type: .system)
-        line2.layer.cornerRadius = 20
-        line2.backgroundColor = .lightGray
-        line2.configuration?.cornerStyle = .capsule
-        line2.heightAnchor.constraint(equalToConstant: 2).isActive = true
-        return line2
-    }()
+    private lazy var line2 = createButton(
+        title: nil,
+        textColor: nil,
+        backgroundColor: .lightGray,
+        icon: nil
+    )
     
-    private lazy var connectWithnLabel: UILabel = {
-        let connectWithnLabel = UILabel()
-        connectWithnLabel.text = "or connect with"
-        connectWithnLabel.textColor = .lightGray
-        connectWithnLabel.font = .systemFont(ofSize: 16, weight: .regular)
-        connectWithnLabel.textAlignment = .center
-        connectWithnLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        connectWithnLabel.numberOfLines = 0
-        return connectWithnLabel
-    }()
+    private lazy var connectWithnLabel = createLabel(
+        text: "or connect with",
+        fontSize: 16,
+        color: .lightGray,
+        alignment: .center
+    )
     
     //MARK: - Connect Buttons Stack
     
-    private lazy var connectButtonStack: UIStackView = {
-        let connectButtonStack = UIStackView(arrangedSubviews: [facebookButton,twitterButton])
-        connectButtonStack.axis = .horizontal
-        connectButtonStack.alignment = .fill
-        connectButtonStack.distribution = .fillEqually
-        connectButtonStack.spacing = 10
-        connectButtonStack.contentMode = .scaleToFill
-        connectButtonStack.translatesAutoresizingMaskIntoConstraints = false
-        return connectButtonStack
-    }()
+    private lazy var connectButtonStack = createStackView(
+        axis: .horizontal,
+        distribution: .fillEqually,
+        spacing: 10,
+        alignment: .fill,
+        contentMode: .scaleToFill
+    )
     
-    private lazy var facebookButton: UIButton = {
-        let facebookButton = UIButton(type: .system)
-        facebookButton.setTitle("Facebook", for: .normal)
-        facebookButton.setTitleColor(.white, for: .normal)
-        facebookButton.backgroundColor = #colorLiteral(red: 0.2073602676, green: 0.644135654, blue: 0.9379553199, alpha: 1)
-        let image = UIImage(named: "facebook")?.withRenderingMode(.alwaysOriginal)
-        facebookButton.layer.cornerRadius = 20
-        facebookButton.setImage(image, for: .normal)
-        facebookButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        return facebookButton
-    }()
-
-    private lazy var twitterButton: UIButton = {
-        let twitterButton = UIButton(type: .system)
-        twitterButton.setTitle("Twitter", for: .normal)
-        twitterButton.setTitleColor(.white, for: .normal)
-        twitterButton.backgroundColor = #colorLiteral(red: 0.3609595895, green: 0.4577199221, blue: 0.7171906829, alpha: 1)
-        twitterButton.tintColor = .white
-        twitterButton.layer.cornerRadius = 20
-        twitterButton.setImage(UIImage(named: "twitter"), for: .normal)
-        twitterButton.heightAnchor.constraint(equalToConstant: 40).isActive = true
-        return twitterButton
-    }()
+    private lazy var facebookButton = createButton(
+        title: "Facebook",
+        textColor: .white,
+        backgroundColor: #colorLiteral(red: 0.2073602676, green: 0.644135654, blue: 0.9379553199, alpha: 1),
+        icon: .facebook.withRenderingMode(.alwaysOriginal)
+    )
+    
+    private lazy var twitterButton = createButton(
+        title: "Twitter",
+        textColor: .white,
+        backgroundColor: #colorLiteral(red: 0.3609595895, green: 0.4577199221, blue: 0.7171906829, alpha: 1),
+        icon: .twitter.withRenderingMode(.alwaysOriginal)
+    )
     
     //MARK: - Sign Up Stack
     
-    private lazy var signUpStack: UIStackView = {
-        let signUpStack = UIStackView(arrangedSubviews: [signUpLabel, signUpButton])
-        signUpStack.axis = .horizontal
-        signUpStack.alignment = .fill
-        signUpStack.distribution = .fill
-        signUpStack.spacing = 0
-        signUpStack.contentMode = .scaleToFill
-        signUpStack.translatesAutoresizingMaskIntoConstraints = false
-        return signUpStack
-    }()
+    private lazy var signUpStack = createStackView(
+        axis: .horizontal,
+        distribution: .fill,
+        spacing: 0,
+        alignment: .fill,
+        contentMode: .scaleToFill
+    )
     
-    private lazy var signUpLabel: UILabel = {
-        let signUpLabel = UILabel()
-        signUpLabel.text = "Don't have account?"
-        signUpLabel.textColor = .lightGray
-        signUpLabel.font = .systemFont(ofSize: 16, weight: .regular)
-        signUpLabel.textAlignment = .center
-        signUpLabel.numberOfLines = 0
-        return signUpLabel
-    }()
+    private lazy var signUpLabel: UILabel = createLabel(
+        text: "Don't have account?",
+        fontSize: 16,
+        color: .lightGray,
+        alignment: .center
+    )
     
-    private lazy var signUpButton: UIButton = {
-        let signUpButton = UIButton(type: .system)
-        signUpButton.setTitle("Sign Up", for: .normal)
-        return signUpButton
-    }()
-    
+    private lazy var signUpButton = createButton(
+        title: "Sign Up",
+        textColor: nil,
+        backgroundColor: nil,
+        icon: nil
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
