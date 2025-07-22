84af6ace641f8e1a071da8cdd7c8f63624ad0bce
DwightShrut <skatemax@mail.ru>
2025-07-23 01:01:22 +0300

    extra hyphenations have been removed

--- iOS1-HW17-MaxMakeevV2/ViewController.swift
+++ iOS1-HW17-MaxMakeevV2/ViewController.swift
@@ -325,24 +325,22 @@ class ViewController: UIViewController {
 //MARK: - Extension to Textfield
 
 extension UITextField {
-    func setLeftIcon(
-        _ image: UIImage) {
-            let iconView = UIImageView(frame: CGRect(x: 20, y: 5, width: 20, height: 20))
-            iconView.image = image
-            let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 60, height: 30))
-            iconContainerView.addSubview(iconView)
-            leftView = iconContainerView
-            leftViewMode = .always
-        }
+    func setLeftIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: 20, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 20, y: 0, width: 60, height: 30))
+        iconContainerView.addSubview(iconView)
+        leftView = iconContainerView
+        leftViewMode = .always
+    }
     
-    func setRightIcon(
-        _ image: UIImage) {
-            let iconView = UIImageView(frame: CGRect(x: -10, y: 5, width: 20, height: 20))
-            iconView.image = image
-            let iconContainerView: UIView = UIView(frame: CGRect(x: 100, y: 0, width: 30, height: 30))
-            iconContainerView.addSubview(iconView)
-            rightView = iconContainerView
-            rightViewMode = .always
-        }
+    func setRightIcon(_ image: UIImage) {
+        let iconView = UIImageView(frame: CGRect(x: -10, y: 5, width: 20, height: 20))
+        iconView.image = image
+        let iconContainerView: UIView = UIView(frame: CGRect(x: 100, y: 0, width: 30, height: 30))
+        iconContainerView.addSubview(iconView)
+        rightView = iconContainerView
+        rightViewMode = .always
+    }
 }
 
